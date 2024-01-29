ed798b4e5d012af501ced30f32476873b43e2a62
Dash <89717536+anthempart2@users.noreply.github.com>
2024-01-29 10:34:29 +0530

    Updated!

---
+++ js_conc_1.js
@@ -0,0 +1,31 @@
+/*we discuss about variables and data types.*/
+
+var old_var = 'hello 1';
+
+//for variables that can be re assigned
+let mutable_var = 'it can change';
+//for those that cannot
+const constant_var = 'I am a constant!';
+
+let age = 25; //int
+let name = 'John'; //string
+let isStudent = true; //boolean
+
+//key-value pairs
+let person = {
+    firstName : 'Mike',
+    lastName : 'Ehrmantraut'
+};
+
+//Arrays
+let colors = ['red', 'green', 'blue'];
+
+//functions
+function greet() {
+    return 'Bye';
+}
+
+//
+let dynamic_var = 'its a string!';
+dynamic_var = 1421; //data type changed from string to var
+
